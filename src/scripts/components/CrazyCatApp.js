178cc2d911f9490ee6b71381a58b3063589d5f07
Liam Ma <liam.q.ma@gmail.com>
2015-03-29 22:12:10 +1100

    first commit

---
+++ src/scripts/components/CrazyCatApp.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var React = require('react/addons');
+var ReactTransitionGroup = React.addons.TransitionGroup;
+
+// CSS
+require('../../styles/normalize.css');
+require('../../styles/main.css');
+
+var imageURL = require('../../images/yeoman.png');
+
+var CrazyCatApp = React.createClass({
+  render: function() {
+    return (
+      <div className='main'>
+        <ReactTransitionGroup transitionName="fade">
+          <img src={imageURL} />
+        </ReactTransitionGroup>
+      </div>
+    );
+  }
+});
+React.render(<CrazyCatApp />, document.getElementById('content')); // jshint ignore:line
+
+module.exports = CrazyCatApp;
